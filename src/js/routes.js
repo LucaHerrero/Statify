01d38341da76b31520e14623ddc2ad5c383b83d0
Luca Herrero <lucaherreromarcos@gmail.com>
2021-11-23 14:23:12 +0100

    added albumView + playlist updated

--- src/js/routes.js
+++ src/js/routes.js
@@ -7,10 +7,9 @@ import ProductPage from '../pages/product.f7';
 import ProfilePage from '../pages/mainView/profile.f7';
 import SearchPage from '../pages/mainView/search.f7'
 
-
+import AlbumPage from '../pages/album.f7';
 import addPlaylist from '../pages/add/playlist.f7'
 import DynamicRoutePage from '../pages/dynamic-route.f7';
-import RequestAndLoad from '../pages/request-and-load.f7';
 import NotFoundPage from '../pages/404.f7';
 
 import TrackPage from '../pages/trackView.f7';
@@ -59,6 +58,44 @@ var routes = [
     },
   },
   {
+    path: '/album/:albumId/',
+    async: function ({ router, to, resolve }) {
+      var app = router.app;
+      var store = app.store;
+      var albumId = to.params.albumId;
+      var spotify = store.getters.spotifyApi.value;
+
+      app.preloader.show();
+
+      spotify
+        .getAlbum(albumId)
+        .then(
+          function (data) {
+            app.preloader.hide();
+            
+            resolve(
+              {
+                component: AlbumPage,
+              },
+              {
+                props: {
+                  album: data,
+                }
+              }
+            );
+          },
+          function (err) {
+            console.log(err)
+            app.preloader.hide();
+
+            app.dialog.alert('An error occurred when loading the data.', 'Sorry 😔', function () {
+              router.back();
+            });
+          }
+        );
+    },
+  },
+  {
     path: '/about/',
     component: AboutPage,
   },
@@ -91,53 +128,6 @@ var routes = [
   {
     path: '/dynamic-route/blog/:blogId/post/:postId/',
     component: DynamicRoutePage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-      console.log(app.store)
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
   },
   {
     path: '(.*)',
