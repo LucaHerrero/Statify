33401d2c34c75c39a84256c8598964c69f4c6bec
Luca Herrero <lucaherreromarcos@gmail.com>
2021-11-25 11:01:00 +0100

    add manifest

--- src/js/app.js
+++ src/js/app.js
@@ -80,3 +80,17 @@ function reportWindowSize() {
 }
 
 window.onresize = reportWindowSize;
+
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  console.log(12)
+  this.setState({ showInstallMessage: true });
+}
