7f8a8fe6999d9081f4210745b7ef723c125bb49d
Luca Herrero <lucaherreromarcos@gmail.com>
2021-11-29 21:54:52 +0100

    update

--- src/js/app.js
+++ src/js/app.js
@@ -69,12 +69,6 @@ var app = new Framework7({
   component: App,
   tokenMap: tokenMap,
   store: store,
-  view: {
-    browserHistory: true,
-    browserHistoryAnimateOnLoad: true,
-    browserHistoryRoot: window.location.origin,
-    browserHistorySeparator: ""
-  },
   routes: routes,
   on: {
     pageInit: function () {}
